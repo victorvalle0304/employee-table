4152524fe2f200d837fb7eb30eac08c4b0d6bf8e
Victor Valle Rodriguez <vihuvr0304@gmail.com>
2021-05-19 17:29:10 -0500

    feat(project): add the employee table with the specifications

--- src/components/CustomTable.js
+++ src/components/CustomTable.js
@@ -1,43 +1,10 @@
-import React, { forwardRef, useState, useEffect } from "react"
-import { Link } from "gatsby"
+import React, { useState} from "react"
 import MaterialTable from "material-table"
-//table icons
-import AddBox from '@material-ui/icons/AddBox';
-import ArrowDownward from '@material-ui/icons/ArrowDownward';
-import Check from '@material-ui/icons/Check';
-import ChevronLeft from '@material-ui/icons/ChevronLeft';
-import ChevronRight from '@material-ui/icons/ChevronRight';
-import Clear from '@material-ui/icons/Clear';
-import DeleteOutline from '@material-ui/icons/DeleteOutline';
-import Edit from '@material-ui/icons/Edit';
-import FilterList from '@material-ui/icons/FilterList';
-import FirstPage from '@material-ui/icons/FirstPage';
-import LastPage from '@material-ui/icons/LastPage';
-import Remove from '@material-ui/icons/Remove';
-import SaveAlt from '@material-ui/icons/SaveAlt';
-import Search from '@material-ui/icons/Search';
-import ViewColumn from '@material-ui/icons/ViewColumn';
+import tableIcons from "./TableIcons"
+import SwitchButton from "./SwitchButton"
+import "./custom-table.css"
 
-const tableIcons = {
-  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
-  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
-  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
-  DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
-  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
-  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
-  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-  PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
-  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
-  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
-  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
-};
-
+const EXCHANGE = 21.50;
 const columns = [
   {
     title: "ID",
@@ -48,46 +15,92 @@ const columns = [
     field: "name",
   },
   {
+    title: "Company",
+    field: "company",
+  },
+  {
     title: "Salary",
     field: "salary",
-    type: "numeric"
+    type: "currency",   
+     cellStyle: (e, rowData) => {
+      if (rowData.salary>=10000) {
+        return { color: "green" };
+      }
+      else  {
+        return { color: "red" };
+      }
+    },    
   },
 ];
 const title = "Employee table"
 
-export default function CustomTable() {
+const tableOptions = {
+  search: true,
+  actionsColumnIndex: -1,
+}
+
+
+export default function CustomTable() {  
+
+  const [checked, setChecked] = useState(true);    
   const [data, setData] = useState(
     [
-      { id: 1, name: "Employee 1", salary: 20000, },
-      { id: 2, name: "Employee 2", salary: 20000, },
-      { id: 3, name: "Employee 3", salary: 20000, },
-      { id: 4, name: "Employee 3", salary: 20000, },
-      { id: 5, name: "Employee 4", salary: 20000, },
-      { id: 6, name: "Employee 5", salary: 20000, },
-      { id: 7, name: "Employee 7", salary: 20000, },
-      { id: 8, name: "Employee 8", salary: 20000, },
-      { id: 9, name: "Employee 9", salary: 20000, },
-      { id: 10, name: "Employee 10", salary: 20000, },
-      { id: 11, name: "Employee 11", salary: 20000, },
-      { id: 12, name: "Employee 12", salary: 20000, },
-      { id: 13, name: "Employee 13", salary: 20000, },
-    ]
-
-  )
-
+      { id: 1, name: "Employee 1", company: "C1", salary: 20000 },
+      { id: 2, name: "Employee 2", company: "C2", salary: 20000 },
+      { id: 3, name: "Employee 3", company: "C3", salary: 20000 },
+      { id: 4, name: "Employee 3", company: "C4", salary: 15000 },
+      { id: 5, name: "Employee 4", company: "C5", salary: 20000 },
+      { id: 6, name: "Employee 5", company: "C6", salary: 20000 },
+      { id: 7, name: "Employee 7", company: "C7", salary: 20000 },
+      { id: 8, name: "Employee 8", company: "C8", salary: 20000 },
+      { id: 9, name: "Employee 9", company: "C9", salary: 20000 },
+      { id: 10, name: "Employee 10", company: "C10", salary: 20000 },
+      { id: 11, name: "Employee 11", company: "C11", salary: 20000 },
+      { id: 12, name: "Employee 12", company: "C12", salary: 20000 },
+      { id: 13, name: "Employee 13", company: "C13", salary: 20000 },
+    ]  )
+       
+  
+  const updateCurrency = () =>{
+    const temporalData = [...data];   
+    temporalData.forEach((element,index)=>{
+      if (checked){
+        let formated = element.salary / EXCHANGE
+        element.salary = formated
+      }
+      else{
+        let formated = element.salary * EXCHANGE
+        element.salary = formated
+      }        
+      console.log(element)
+    })    
+  }
+  
+  const handleSwitchChange = (event) => { 
+    updateCurrency()
+    setChecked(event.target.checked)
+    console.log(checked, "actual")
+  };  
   return (
     <div className="">
-      <MaterialTable
+      <SwitchButton checked={checked} onChange={handleSwitchChange}/>
+      <span>Number of rows: {data.length}</span>
+      <MaterialTable  
+        className="test"      
         columns={columns}
         data={data}
         title={title}
-        icons={tableIcons}
-        options={{
-          search: true,
-          actionsColumnIndex: -1,
-        }}
+        icons={tableIcons} 
+        actions={[
+          {
+            icon: tableIcons.AddPhotoAlternateIcon,
+            tooltip: 'Upload Photo',
+            onClick: (event, rowData) => alert("You saved " + rowData.name)
+          },        
+        ]}
+        options={tableOptions}
         editable={{
-          // isEditable: rowData => rowData.name === 'a', // only name(a) rows would be editable
+            // isEditable: rowData => rowData.name === 'a', // only name(a) rows would be editable
           //isEditHidden: rowData => rowData.name === 'x',
           //isDeletable: rowData => rowData.name === 'b', // only name(b) rows would be deletable,
           //isDeleteHidden: rowData => rowData.name === 'y',          
@@ -122,7 +135,6 @@ export default function CustomTable() {
               }, 1000);
             })
         }}
-
       />
     </div>
   )
